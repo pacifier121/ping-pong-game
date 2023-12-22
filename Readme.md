16eb097e63409b6e9cb8a779a1b3481da77a7767
Prashant Rawat <prashantrawat2mx@gmail.com>
2023-12-23 03:36:09 +0530

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# A simple PingPong Game made with Vanilla JS, HTML and CSS
---
+++ script.js
@@ -0,0 +1,102 @@
+const ball = document.getElementById('ball')
+const bobBar = document.getElementById('bobBar')
+const aliceBar = document.getElementById('aliceBar')
+const celebrationElem = document.getElementById('celebration');
+
+const barSpeed = 5;
+const initialX = 725, initialY = 375;
+
+let posBob = 300, posAlice = 300;
+let velX = 10, velY = 10;
+let ballX = initialX, ballY = initialY;
+
+let keys = {
+    "w": false,
+    "s": false,
+    "ArrowUp": false,
+    "ArrowDown": false
+}
+
+// For Bob
+document.addEventListener("keydown", (e) => {
+    if (e.key === "w") keys["w"] = true;
+    else if (e.key === "s") keys["s"] = true;
+})
+document.addEventListener("keyup", (e) => {
+    if (e.key === "w") keys["w"] = false;
+    else if (e.key === "s") keys["s"] = false;
+})
+
+// For Alice
+document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowUp") keys["ArrowUp"] = true;
+    else if (e.key === "ArrowDown") keys['ArrowDown'] = true;
+})
+document.addEventListener("keyup", (e) => {
+    if (e.key === "ArrowUp") keys["ArrowUp"] = false;
+    else if (e.key === "ArrowDown") keys["ArrowDown"] = false;
+})
+
+// To restart the game
+document.addEventListener("keydown", (e) => {
+    if (e.key === " ") resetGame();
+})
+
+// Gameloop
+let gameLoop = setInterval(resumeGame, 40);
+
+
+// Helper functions
+function resetGame(){
+    posBob = 300; posAlice = 300;
+    velX = 10; velY = 10;
+    ballX = initialX; ballY = initialY;
+
+    keys = {
+        "w": false,
+        "s": false,
+        "ArrowUp": false,
+        "ArrowDown": false
+    }
+    celebrationElem.style.opacity = 0;
+    gameLoop = setInterval(resumeGame, 40);
+}
+
+function resumeGame() {
+    if (keys["w"]) posBob = Math.max(0, posBob - barSpeed);
+    if (keys["s"]) posBob = Math.min(600, posBob + barSpeed);
+
+    if (keys["ArrowUp"]) posAlice = Math.max(0, posAlice - barSpeed);
+    if (keys["ArrowDown"]) posAlice = Math.min(600, posAlice + barSpeed);
+
+    bobBar.style.top = posBob + "px";
+    aliceBar.style.top = posAlice + "px";
+
+    checkCollision();
+    moveBall();
+}
+
+function moveBall() {
+    ball.style.left = ballX + "px";
+    ball.style.top = ballY + "px";
+    ballX += velX;
+    ballY += velY;
+}
+
+function checkCollision() {
+    if (ballY <= 0 || ballY >= 750) velY *= -1;
+    if (ballX <= 0) celebration("Red");     // Alice is Player Red
+    if (ballX >= 1450) celebration("Blue");     // Bob is Player Blue
+
+    // Check ball's collistion with Bob's bar
+    if (ballX <= 50 && (posBob <= ballY && ballY <= posBob+200)) velX *= -1;
+
+    // Check ball's collistion with Alice's bar
+    if (ballX >= 1400 && (posAlice <= ballY && ballY <= posAlice+200)) velX *= -1;
+}
+
+function celebration(winner){
+    clearInterval(gameLoop);
+    celebrationElem.innerText = `${winner} won!`;
+    celebrationElem.style.opacity = 1; 
+}
